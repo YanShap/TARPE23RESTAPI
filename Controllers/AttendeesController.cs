9544d2c0e40c256f05b081646f669a5f5f56a506
Opilane TTHK <opilane@tthk.ee>
2025-02-14 11:26:16 +0200

    Started with controller

--- Controllers/AttendeesController.cs
+++ Controllers/AttendeesController.cs
@@ -1,12 +1,122 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITB2203Application.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ITB2203Application.Controllers
 {
-    public class AtendeesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttendeesController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly DataContext _context;
+
+        public AttendeesController(DataContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Attendee>> GetAttendees(int? daysBeforeEvent = null)
+        {
+            var query = _context.Attendees.AsQueryable();
+
+            if (daysBeforeEvent.HasValue)
+            {
+                query = query.Where(a =>
+                    _context.Events
+                        .Where(e => e.Id == a.EventId)
+                        .Any(e => a.RegistrationTime < e.Date.AddDays(-daysBeforeEvent.Value))
+                );
+            }
+
+            return Ok(query.ToList());
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Attendee> GetAttendee(int id)
+        {
+            var attendee = _context.Attendees!.Find(id);
+
+            if (attendee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(attendee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutAttendee(int id, Attendee attendee)
+        {
+            if (id != attendee.Id)
+            {
+                return BadRequest("ID mismatch.");
+            }
+
+            var existingAttendee = _context.Attendees!.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (existingAttendee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Update(attendee);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<Attendee> PostAttendee(Attendee attendee)
+        {
+            var eventDetails = _context.Events!.FirstOrDefault(e => e.Id == attendee.EventId);
+            if (eventDetails == null)
+            {
+                return NotFound("Event not found.");
+            }
+
+            var speaker = _context.Speakers!.FirstOrDefault(s => s.Id == eventDetails.SpeakerId);
+
+            if (!attendee.Email.Contains('@'))
+            {
+                return BadRequest("Invalid email format.");
+            }
+
+            if (attendee.RegistrationTime >= eventDetails.Date)
+            {
+                return BadRequest("Registration time cannot be after event date.");
+            }
+
+            if (_context.Attendees!.Any(a => a.Email == attendee.Email))
+            {
+                return BadRequest("Attendee with this email already exists.");
+            }
+
+            if (speaker != null && speaker.Email == attendee.Email)
+            {
+                return BadRequest("Speaker cannot register as an attendee.");
+            }
+
+            attendee.Id = 0;
+            _context.Attendees.Add(attendee);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetAttendee), new { id = attendee.Id }, attendee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAttendee(int id)
+        {
+            var attendee = _context.Attendees!.Find(id);
+            if (attendee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(attendee);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITB2203Application.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ITB2203Application.Controllers
 {
-    public class EventsController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly DataContext _context;
+
+        public EventsController(DataContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Event>> GetEvents(string? name = null, string? location = null)
+        {
+            var query = _context.Events.AsQueryable();
+
+            if (name != null)
+                query = query.Where(x => x.Name != null && x.Name.ToUpper().Contains(name.ToUpper()));
+
+            if (location != null)
+                query = query.Where(x => x.Location != null && x.Location.ToUpper().Contains(location.ToUpper()));
+
+            return query.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Event> GetEvent(int id)
+        {
+            var @event = _context.Events!.Find(id);
+
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@event);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutEvent(int id, Event @event)
+        {
+            var dbEvent = _context.Events.AsNoTracking().FirstOrDefault(x => x.Id == @event.Id);
+            if (id != @event.Id || dbEvent == null)
+            {
+                return NotFound();
+            }
+
+            _context.Update(@event);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<Event> PostEvent(Event @event)
+        {
+            var speakerExists = _context.Speakers!.Any(s => s.Id == @event.SpeakerId);
+            if (!speakerExists)
+            {
+                return NotFound("Speaker not found.");
+            }
+
+            _context.Events!.Add(@event);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetEvent), new { Id = @event.Id }, @event);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEvent(int id)
+        {
+            var @event = _context.Events.Find(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(@event);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
